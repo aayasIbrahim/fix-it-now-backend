a9982b1139b4d116823bc782a8ce35cf550b7a45
Ayas Ibrahim <aayasabraham@gmail.com>
2026-08-13 15:15:53 +0600

    feat: initialize Prisma schema and user model with roles
    - Added enums for user roles (CUSTOMER, TECHNICIAN, ADMIN).
    - Created Prisma schema file with PostgreSQL datasource.
    - Defined User model with fields: id, name, email, password, role, createdAt, and updatedAt.
    - Implemented basic Express server setup with middleware for error handling and authentication.
    - Configured environment variables for application settings.
    - Integrated Prisma client for database operations.
    - Added middleware for global error handling and route not found responses.
    - Established TypeScript configuration for the project.

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import httpStatus from "http-status";
+import config from "./app/config";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
+// import { AuthRoutes } from "./app/module/auth/auth.route";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.frontend_url,
+    credentials: true,
+  }),
+);
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1/auth", () => {});
+
+// Basic route
+app.get("/", async (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Welcome to  Backend",
+    author: "Ayas Ibrahim",
+  });
+});
+
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
